7a68feb2143fb2f884e7ee2000566304ee83e849
dlinch <dlinch33@gmail.com>
2017-08-12 21:31:26 -0600

    initial commit, have projects page skeleton completed

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+};
+</script>
+
+<style>
+@import url('https://fonts.googleapis.com/css?family=Dosis|Quicksand|Saira+Semi+Condensed');
+#app {
+  font-family: 'Dosis', Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: black;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Projects.vue
@@ -0,0 +1,93 @@
+<template>
+  <div class="projects">
+    <h2>Projects</h2>
+    <ul class="project-list">
+      <li v-for="project in projects">
+        <a :href="project.link">{{project.title}}</a>
+        <p class="description">{{project.description}}</p>
+        <p >
+          {{project.technologies.join(", ")}}
+        </p>
+      </li>
+
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'projects',
+  data() {
+    return {
+      projects: [
+        {
+          title: 'Skyffel',
+          link: 'https://skyffel.co',
+          description: 'On demand snow shoveling.',
+          technologies: ['Node', 'Express', 'Knex'],
+        },
+        {
+          title: 'BookToRead',
+          link: 'https://booktoread.herokuapp.com/',
+          description: 'An API for award winning Science Fiction.',
+          technologies: ['Node', 'Express'],
+        },
+        {
+          title: 'Dander',
+          link: 'https://dander.co',
+          description: 'Find your new best friend.',
+          technologies: ['Node', 'Express'],
+        },
+        {
+          title: 'Board and Play',
+          link: 'https://boardandplay.herokuapp.com/',
+          description: 'Track your boardgame statistics.',
+          technologies: ['Ruby', 'Rails'],
+        },
+        {
+          title: 'Bracketer',
+          link: 'https://dlinch.com/projects',
+          description: 'Bracket all the things.',
+          technologies: ['JQuery'],
+        },
+        {
+          title: 'Warble',
+          link: 'https://dlinch.com/projects',
+          description: 'Under wraps.',
+          technologies: ['TBD'],
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style scoped>
+h2 {
+  font-family: 'Quicksand', Arial, sans-serif;
+}
+
+.description {
+  font-family: 'Saira Semi Condensed', Arial, serif;
+}
+.project-list {
+  display: grid;
+  grid-template: 1fr 1fr / 1fr 1fr;
+}
+
+li {
+  list-style: none;
+  margin: 70px;
+}
+
+a {
+  color: rgba(73,203, 134, .9);
+  text-decoration: none;
+}
+
+@media(max-width: 700px) {
+  .project-list {
+    grid-template: 100% / 100%;
+  }
+}
+</style>
